57e72095d0709136448c7399954b4e47640593c2
Monty-01 <111624205+Monty-01@users.noreply.github.com>
2022-12-12 18:27:06 +0530

    Add files via upload

---
+++ start.js
@@ -0,0 +1,8 @@
+<html>
+    <title>Hello World in javascript first time</title>
+    <body>
+        <script>
+            docuument.write("<h2>Hello World in Javascipt</h2>");
+        </script>
+    </body>
+</html>
